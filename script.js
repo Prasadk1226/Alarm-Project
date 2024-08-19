c40eda3e380bd349f354484b48627d0fa0dd5c2a
Prasadk1226 <124857435+Prasadk1226@users.noreply.github.com>
2024-08-19 17:14:47 +0530

    added current time

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let alarmRing = document.getElementsByClassName('ring')
-let timeSet = document.getElementsByClassName('firstAlarm')
+let alarmRing = document.getElementsByClassName("ring");
+let timeSet = document.getElementsByClassName("firstAlarm");
+
+let hrs = document.getElementById("hrs");
+let mnts = document.getElementById("mnts");
+let secs = document.getElementById("secs");
+
+setInterval(() => {
+  let currentTime = new Date();
+
+  hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+  mnts.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+  secs.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+}, 1000);
