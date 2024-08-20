518d8e8cedfb9a9a88aa2c929195f48de9183bce
Prasadk1226 <124857435+Prasadk1226@users.noreply.github.com>
2024-08-20 14:24:58 +0530

    modified code

--- script.js
+++ script.js
@@ -5,32 +5,33 @@ let secs = document.getElementById("secs");
 setInterval(() => {
   let currentTime = new Date();
 
-  hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-  mnts.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
-  secs.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+  hrs.innerHTML =
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  mnts.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  secs.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 }, 1000);
 
-let setAlarm = document.getElementsByClassName("setAlarm")
-let checked = document.getElementsByTagName("input")
-let audio = document.getElementsByTagName("audio")
+// let activeAlarm;
 
-let activeAlarm;
+// document.getElementById("check1").onclick = function(){
+//     activeAlarm = document.getElementById("alarm1").value;
+//     document.getElementById("activeTime").textContent =`Alarm at  ${activeAlarm}`
+// }
 
-document.getElementById("check").onclick = function(){
-    activeAlarm = document.getElementById("alarm1").value;
-    document.getElementById("activeTime").textContent =`Alarm at  ${activeAlarm}`
-}
+// document.getElementById("changeBtn").onclick = function(){
+//  activeAlarm = document.getElementById("alarm1").value;
+//     document.getElementById("activeTime").textContent =`Alarm at  ${activeAlarm}`
+// }
 
-document.getElementById("changeBtn").onclick = function(){
- activeAlarm = document.getElementById("alarm1").value;
-    document.getElementById("activeTime").textContent =`Alarm at  ${activeAlarm}`
-}
-if (setAlarm==checked && setAlarm==(hrs&&mnts)){
-    console.log(audio)
-}
+let check1 = document.getElementById("check1");
 
-
-
-
-
-
+check1.onclick = function () {
+  let activeAlarm = document.getElementById("alarm1").value;
+  if (check1.checked && activeAlarm) {
+    document.getElementById("activeTime").textContent = `Alarm at  ${activeAlarm}`;
+  } else {
+    document.getElementById("activeTime").textContent = "No alarm set.";
+  }
+};
