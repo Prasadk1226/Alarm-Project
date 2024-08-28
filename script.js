2fbb7c72ffd911f1ce5f76581294cf3ab63280b4
Prasadk1226 <124857435+Prasadk1226@users.noreply.github.com>
2024-08-29 01:56:32 +0530

    Changed many things

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let hrs = document.getElementById("hrs");
 let mnts = document.getElementById("mnts");
 let secs = document.getElementById("secs");
+let time = document.getElementById("alarm1")
 
 setInterval(() => {
   let currentTime = new Date();
@@ -11,19 +12,10 @@ setInterval(() => {
     (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
   secs.innerHTML =
     (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+
 }, 1000);
 
-// let activeAlarm;
 
-// document.getElementById("check1").onclick = function(){
-//     activeAlarm = document.getElementById("alarm1").value;
-//     document.getElementById("activeTime").textContent =`Alarm at  ${activeAlarm}`
-// }
-
-// document.getElementById("changeBtn").onclick = function(){
-//  activeAlarm = document.getElementById("alarm1").value;
-//     document.getElementById("activeTime").textContent =`Alarm at  ${activeAlarm}`
-// }
 let alarmSound = new Audio("old_alarm_sound.mp3")
 let check1 = document.getElementById("check1");
 
@@ -31,13 +23,14 @@ check1.onclick = function () {
   let activeAlarm = document.getElementById("alarm1").value;
   if (check1.checked && activeAlarm) {
     document.getElementById("activeTime").textContent = `Next Alarm at  ${activeAlarm}`;
-    
   } else {
     document.getElementById("activeTime").textContent = "No alarm set.";
   }
 };
 
-if(activeTime==currentTime){
-  alarmSound.play;
+if(time==activeAlarm && check1.checked){
+  alarmSound.play();
     alarmSound.loop = true;
+    console.log("Ringing")
 }
+
